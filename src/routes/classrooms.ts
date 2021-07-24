90ee171e5955cbe1009c6a54a9c2162cd14feb17
kgm <68318079+KimGyeongMin1@users.noreply.github.com>
2021-07-24 16:21:06 +0900

    Add: Classroom 엔드포인트

--- src/routes/classrooms.ts
+++ src/routes/classrooms.ts
@@ -11,4 +11,35 @@ router.get('/', async (req, rsp) => {
   rsp.send(rows)
 })
 
+router.get('/all/assignments', async (req, rsp) => {
+  const conn = await getConnection()
+  let r = await conn.execute('SELECT * FROM assignments')
+  let rows = r[0]
+
+  rsp.send(rows)
+})
+
+router.get('/{class_id}/assignments', async (req, rsp) => {
+  const conn = await getConnection()
+  let r = await conn.execute('SELECT * FROM assignments')
+  let rows = r[0]
+
+  rsp.send(rows)
+})
+
+router.get('/{class_id}/boards', async (req, rsp) => {
+  const conn = await getConnection()
+  let r = await conn.execute('SELECT * FROM boards')
+  let rows = r[0]
+
+  rsp.send(rows)
+})
+
+router.get('/{class_id}/boards/{board_id}/list', async (req, rsp) => {
+  const conn = await getConnection()
+  let r = await conn.execute('SELECT * FROM boards')
+  let rows = r[0]
+
+  rsp.send(rows)
+})
 export default router
